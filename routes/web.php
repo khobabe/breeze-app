b7e6563898cf65a1a823e26b070c7f1cb0212d56
khobabe <khobabe007@gmail.com>
2025-02-22 12:04:38 +0530

    support ticket subjects livewire done

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Livewire\Admin\AdminDashboard;
+use App\Livewire\Admin\SupportTicketSubject;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -21,6 +22,7 @@ Route::middleware('auth')->group(function () {
 require __DIR__ . '/auth.php';
 
 
-Route::get('/admin/dashboard', AdminDashboard::class)
-    ->middleware(['auth', 'admin'])
-    ->name('admin.dashboard');
+Route::middleware(['auth', 'admin'])->prefix('admin')->group(function () {
+    Route::get('/', AdminDashboard::class)->name('admin.dashboard');
+    Route::get('/ticket-subject', SupportTicketSubject::class)->name('admin.ticket.subject');
+});
