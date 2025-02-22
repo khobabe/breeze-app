aee2a9855d945aa6d7d6fc2d50b75b80184a9a48
khobabe <khobabe007@gmail.com>
2025-02-22 10:52:42 +0530

    admin dashboard livewire setup done

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Livewire\Admin\AdminDashboard;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -17,4 +18,9 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
+
+
+Route::get('/admin/dashboard', AdminDashboard::class)
+    ->middleware(['auth', 'admin'])
+    ->name('admin.dashboard');
